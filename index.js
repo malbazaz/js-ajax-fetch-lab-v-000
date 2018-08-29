6a93402bf8734bf7774698201f8864410d700718
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-29 13:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
-  fetch('api.github.com/repos/malbazaz/javascript-fetch-lab/forks')
+  fetch('api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks')
 }
 
 function getToken() {
